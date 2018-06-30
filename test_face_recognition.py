79feb5224332aba9dfbfc51b78ceb310685eb523
Nils Schaetti <n.schaetti@gmail.com>
2018-06-30 16:03:33 +0200

    Base and clips

--- test_face_recognition.py
+++ test_face_recognition.py
@@ -1,50 +1,64 @@
 # -*- coding: utf-8 -*-
 #
 
-# Imports
+import face_recognition
 import cv2
-import sys
+import argparse
 
 
-# Classifier path
-class_path = "."
-face_cascade = cv2.CascadeClassifier(class_path)
+# Arguments
+parser = argparse.ArgumentParser(u"Face detection")
+parser.add_argument("--movie", type=str, required=True)
+parser.add_argument("--output", type=str, required=True)
+args = parser.parse_args()
 
-# Get video devices
-video_capture = cv2.VideoCapture(0)
+# Input movie
+input_movie = cv2.VideoCapture(args.movie)
 
-# Capture frame by frame
+# How many frames
+length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
+
+# Create an output movie file
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+output_movie = cv2.VideoWriter('output.avi', fourcc, 25, (596, 336))
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+frame_number = 0
+
 while True:
-    # Get the frame
-    _, frame = video_capture.read()
+    # Grab a single frame of video
+    ret, frame = input_movie.read()
+    frame_number += 1
 
-    # To gray
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Classifier
-    faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.CV_HAAR_SCALE_IMAGE
-    )
-
-    # Draw rectangle around each faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    # end for
-
-    # Display the frame
-    cv2.imshow('Video', frame)
-
-    # If exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Quit when the input video file ends
+    if not ret:
         break
     # end if
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_frame = frame[:, :, ::-1]
+
+    # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(rgb_frame)
+
+    # Label the results
+    for (top, right, bottom, left) in face_locations:
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+    # end for
+
+    # Write the resulting image to the output video file
+    print("Writing frame {} / {}".format(frame_number, length))
+    output_movie.write(frame)
 # end while
 
-# Release
-video_capture.release()
+# All done!
+input_movie.release()
 cv2.destroyAllWindows()
-
