7eedb10db2493e7257a87b310269e9312e6b178f
Nils Schaetti <n.schaetti@gmail.com>
2018-07-01 16:42:16 +0200

    Dataset

--- test_face_recognition.py
+++ test_face_recognition.py
@@ -114,9 +114,10 @@ while True:
 
         # Predict emotion
         _, predicted = torch.max(model(Variable(face_tensor)).data, 1)
-        print(predicted)
-        print(classes[predicted[0]])
-        exit()
+        predicted_emotion = classes[predicted[0]]
+
+        print(predicted_emotion)
+
         # Draw a box around the face
         cv2.circle(frame, face_center, 5, (0, 0, 255))
         cv2.rectangle(
